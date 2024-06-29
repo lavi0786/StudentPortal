e08e0db7921a8eee12f56592c5dd537f6d7ee6ad
lavi0786 <bujjamma.lg@gmail.com>
2024-06-29 23:37:01 +0200

    Add project files.

---
+++ StudentPortal.Web/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentPortal.Web.Models;
+
+namespace StudentPortal.Web.Controllers
+{
+    public class StudentsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddStudentViewModel viewModel)
+        {
+            return View();
+        }
+    }
+}
