d5189744381953a5b8c07ee2e4a04836fa703b3f
lavi <bujjamma.lg@gmail.com>
2024-06-30 16:37:14 +0200

    Revert "Added RUD  out of CRUD features"
    This reverts commit 5bf0b7fa348af3ff6d41ab736af6cbd5f462e416.

--- StudentPortal.Web/Controllers/StudentsController.cs
+++ StudentPortal.Web/Controllers/StudentsController.cs
@@ -1,20 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using StudentPortal.Web.Data;
 using StudentPortal.Web.Models;
-using StudentPortal.Web.Models.Entities;
-using System.ComponentModel.DataAnnotations;
 
 namespace StudentPortal.Web.Controllers
 {
     public class StudentsController : Controller
     {
-        private readonly ApplicationDbContext dbContext;
-        public StudentsController(ApplicationDbContext dbContext)
-        {
-            this.dbContext = dbContext;
-        }
-
         [HttpGet]
         public IActionResult Add()
         {
@@ -22,66 +12,9 @@ namespace StudentPortal.Web.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
+        public IActionResult Add(AddStudentViewModel viewModel)
         {
-            var student = new Student
-            {
-                Name = viewModel.Name,
-                Email = viewModel.Email,
-                Phone = viewModel.Phone,
-                Subscribed = viewModel.Subscribed
-            };
-
-            await dbContext.Students.AddAsync(student);
-            await dbContext.SaveChangesAsync();
             return View();
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> List()
-        {
-            var students = await dbContext.Students.ToListAsync();
-            return View(students);
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Edit(Guid id)
-        {
-            var student = await dbContext.Students.FindAsync(id);
-            return View(student);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Edit(Student viewModel)
-        {
-            var student = await dbContext.Students.FindAsync(viewModel.Id);
-
-            if (student is not null)
-            {
-                student.Name = viewModel.Name;
-                student.Email = viewModel.Email;
-                student.Phone = viewModel.Phone;
-                student.Subscribed = viewModel.Subscribed;
-
-                await dbContext.SaveChangesAsync();
-            }
-            return RedirectToAction("List", "Students");
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Delete(Student viewModel)
-        {
-            var student = await dbContext.Students
-                .AsNoTracking()
-                .FirstOrDefaultAsync(x => x.Id == viewModel.Id);
-
-            if(student is not null)
-            {
-                dbContext.Students.Remove(viewModel);
-                await dbContext.SaveChangesAsync();
-            }
-
-            return RedirectToAction("List", "Students");
         }
     }
 }
